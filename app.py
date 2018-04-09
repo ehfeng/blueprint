b5a0767dcd0a890d0bb377a98a2618d50c5906e2
Eric Feng <erichfeng@gmail.com>
2018-04-08 20:13:58 -0700

    initial commit
    got a minimal example working from http://flask.pocoo.org/docs/0.12/patterns/celery/

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask
+
+app = Flask(__name__)
+app.config.update(
+    CELERY_BROKER_URL='redis://localhost:6379',
+    CELERY_RESULT_BACKEND='redis://localhost:6379'
+)
+
+from celery import Celery
+
+def make_celery(app):
+    celery = Celery(app.import_name, backend=app.config['CELERY_RESULT_BACKEND'],
+                    broker=app.config['CELERY_BROKER_URL'])
+    celery.conf.update(app.config)
+    TaskBase = celery.Task
+    class ContextTask(TaskBase):
+        abstract = True
+        def __call__(self, *args, **kwargs):
+            with app.app_context():
+                return TaskBase.__call__(self, *args, **kwargs)
+    celery.Task = ContextTask
+    return celery
+
+celery = make_celery(app)
+
+
+@celery.task()
+def add_together(a, b):
+    print('Hello, Celery!')
+    return a + b
+
+
+@app.route('/')
+def index():
+    add_together.delay(1, 2)
+    return 'hi'
